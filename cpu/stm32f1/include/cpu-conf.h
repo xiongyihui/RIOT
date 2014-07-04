9ba6262af0bf8ad3b34606b6b557e896e0c27a34
Thomas Eichinger <thomas.eichinger1@gmail.com>
2014-07-04 11:11:54 +0200

    adjusted stack sizes, builds rpl_udp example now

--- cpu/stm32f1/include/cpu-conf.h
+++ cpu/stm32f1/include/cpu-conf.h
@@ -31,13 +31,13 @@
  * TODO: measure and adjust for the cortex-m3
  * @{
  */
-#define KERNEL_CONF_STACKSIZE_PRINTF    (2500)
+#define KERNEL_CONF_STACKSIZE_PRINTF    (1024)
 
 #ifndef KERNEL_CONF_STACKSIZE_DEFAULT
-#define KERNEL_CONF_STACKSIZE_DEFAULT   (2500)
+#define KERNEL_CONF_STACKSIZE_DEFAULT   (512)
 #endif
 
-#define KERNEL_CONF_STACKSIZE_IDLE      (512)
+#define KERNEL_CONF_STACKSIZE_IDLE      (256)
 /** @} */
 
 /**
