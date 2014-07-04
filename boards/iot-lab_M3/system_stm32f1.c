65cc4a60a8c29349e10e999f434c2d129f3d0160
Thomas Eichinger <thomas.eichinger1@gmail.com>
2014-07-04 12:02:23 +0200

    implement own System* functions

---
+++ boards/iot-lab_M3/system_stm32f1.c
@@ -0,0 +1,180 @@
+/**
+ * Copyright (C) 2014 Freie Universität Berlin
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; version 2.1 of the License.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * @file   system_stm32f1.c
+ * @author Thomas Eichinger <thomas.eichinger@fu-berlin.de>
+ */
+
+#include "stm32f10x.h"
+#include "board.h"
+
+uint32_t SystemCoreClock = F_CPU;
+
+#define VECT_TAB_OFFSET  0x0
+
+__I uint8_t AHBPrescTable[16] = {0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 6, 7, 8, 9};
+
+static void SetSysClock(void);
+
+void SystemInit(void)
+{
+    /* Reset the RCC clock configuration to the default reset state(for debug purpose) */
+    /* Set HSION bit */
+    RCC->CR |= (uint32_t)0x00000001;
+
+    /* Reset SW, HPRE, PPRE1, PPRE2, ADCPRE and MCO bits */
+    RCC->CFGR &= (uint32_t)0xF0FF0000;
+
+    /* Reset HSEON, CSSON and PLLON bits */
+    RCC->CR &= (uint32_t)0xFEF6FFFF;
+
+    /* Reset HSEBYP bit */
+    RCC->CR &= (uint32_t)0xFFFBFFFF;
+
+    /* Reset PLLSRC, PLLXTPRE, PLLMUL and USBPRE/OTGFSPRE bits */
+    RCC->CFGR &= (uint32_t)0xFF80FFFF;
+
+    /* Disable all interrupts and clear pending bits  */
+    RCC->CIR = 0x009F0000;
+
+    /* Configure the System clock frequency, HCLK, PCLK2 and PCLK1 prescalers */
+    /* Configure the Flash Latency cycles and enable prefetch buffer */
+    SetSysClock();
+
+    SCB->VTOR = FLASH_BASE | VECT_TAB_OFFSET; /* Vector Table Relocation in Internal FLASH. */
+}
+
+void SystemCoreClockUpdate(void)
+{
+    uint32_t tmp = 0, pllmull = 0, pllsource = 0;
+    /* Get SYSCLK source -------------------------------------------------------*/
+    tmp = RCC->CFGR & RCC_CFGR_SWS;
+
+    switch (tmp) {
+        case 0x00:  /* HSI used as system clock */
+            SystemCoreClock = HSI_VALUE;
+            break;
+
+        case 0x04:  /* HSE used as system clock */
+            SystemCoreClock = HSE_VALUE;
+            break;
+
+        case 0x08:  /* PLL used as system clock */
+
+            /* Get PLL clock source and multiplication factor ----------------------*/
+            pllmull = RCC->CFGR & RCC_CFGR_PLLMULL;
+            pllsource = RCC->CFGR & RCC_CFGR_PLLSRC;
+            pllmull = (pllmull >> 18) + 2;
+
+            if (pllsource == 0x00) {
+                /* HSI oscillator clock divided by 2 selected as PLL clock entry */
+                SystemCoreClock = (HSI_VALUE >> 1) * pllmull;
+            }
+            else {
+                /* HSE selected as PLL clock entry */
+                if ((RCC->CFGR & RCC_CFGR_PLLXTPRE) != (uint32_t)RESET) {
+                    /* HSE oscillator clock divided by 2 */
+                    SystemCoreClock = (HSE_VALUE >> 1) * pllmull;
+                }
+                else {
+                    SystemCoreClock = HSE_VALUE * pllmull;
+                }
+            }
+            break;
+
+        default:
+            SystemCoreClock = HSI_VALUE;
+            break;
+    }
+
+    /* Compute HCLK clock frequency */
+    /* Get HCLK prescaler */
+    tmp = AHBPrescTable[((RCC->CFGR & RCC_CFGR_HPRE) >> 4)];
+    /* HCLK clock frequency */
+    SystemCoreClock >>= tmp;
+}
+
+static void SetSysClock(void)
+{
+    __IO uint32_t StartUpCounter = 0, HSEStatus = 0;
+
+    /* SYSCLK, HCLK, PCLK2 and PCLK1 configuration */
+    /* Enable HSE */
+    RCC->CR |= ((uint32_t)RCC_CR_HSEON);
+
+    /* Wait till HSE is ready and if Time out is reached exit */
+    do {
+        HSEStatus = RCC->CR & RCC_CR_HSERDY;
+        StartUpCounter++;
+    }
+    while ((HSEStatus == 0) && (StartUpCounter != HSE_STARTUP_TIMEOUT));
+
+    if ((RCC->CR & RCC_CR_HSERDY) != RESET) {
+        HSEStatus = (uint32_t)0x01;
+    }
+    else {
+        HSEStatus = (uint32_t)0x00;
+    }
+
+    if (HSEStatus == (uint32_t)0x01) {
+        /* Enable Prefetch Buffer */
+        FLASH->ACR |= FLASH_ACR_PRFTBE;
+
+        /* Flash 2 wait state */
+        FLASH->ACR &= (uint32_t)((uint32_t)~FLASH_ACR_LATENCY);
+        FLASH->ACR |= (uint32_t)FLASH_ACR_LATENCY_2;
+
+
+        /* HCLK = SYSCLK */
+        RCC->CFGR |= (uint32_t)RCC_CFGR_HPRE_DIV1;
+
+        /* PCLK2 = HCLK */
+        RCC->CFGR |= (uint32_t)RCC_CFGR_PPRE2_DIV1;
+
+        /* PCLK1 = HCLK */
+        RCC->CFGR |= (uint32_t)RCC_CFGR_PPRE1_DIV2;
+
+        /* NOTE : agilefox : modified to take into account the 16MHz
+                             crystal instead of 8MHz */
+        /*  PLL configuration: PLLCLK = HSE / 2  * 9 = 72 MHz */
+        RCC->CFGR &= (uint32_t)((uint32_t)~(RCC_CFGR_PLLSRC
+                                            | RCC_CFGR_PLLXTPRE
+                                            | RCC_CFGR_PLLMULL));
+
+        RCC->CFGR |= (uint32_t)(RCC_CFGR_PLLSRC_HSE
+                                | RCC_CFGR_PLLXTPRE_HSE_Div2
+                                | RCC_CFGR_PLLMULL9);
+
+        /* Enable PLL */
+        RCC->CR |= RCC_CR_PLLON;
+
+        /* Wait till PLL is ready */
+        while ((RCC->CR & RCC_CR_PLLRDY) == 0) {
+        }
+
+        /* Select PLL as system clock source */
+        RCC->CFGR &= (uint32_t)((uint32_t)~(RCC_CFGR_SW));
+        RCC->CFGR |= (uint32_t)RCC_CFGR_SW_PLL;
+
+        /* Wait till PLL is used as system clock source */
+        while ((RCC->CFGR & (uint32_t)RCC_CFGR_SWS) != (uint32_t)0x08) {
+        }
+    }
+    else {
+        /* If HSE fails to start-up, the application will have wrong clock
+           configuration. User can add here some code to deal with this error */
+    }
+}
