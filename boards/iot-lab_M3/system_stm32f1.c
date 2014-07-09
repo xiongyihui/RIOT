98c85ee48beb9599b65076a1b8366cc5cd47fea1
Thomas Eichinger <thomas.eichinger1@gmail.com>
2014-07-09 15:36:16 +0200

    fix licenses (again)

--- boards/iot-lab_M3/system_stm32f1.c
+++ boards/iot-lab_M3/system_stm32f1.c
@@ -1,19 +1,9 @@
 /**
  * Copyright (C) 2014 Freie Universität Berlin
  *
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; version 2.1 of the License, or (at your option) any
- * later version.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License v2.1. See the file LICENSE in the top level directory for more
  * details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  *
  * @file   system_stm32f1.c
  * @author Thomas Eichinger <thomas.eichinger@fu-berlin.de>
