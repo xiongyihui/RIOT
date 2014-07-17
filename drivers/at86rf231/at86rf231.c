5337d507b94d9e4edfdfd0997bd6e5e8c10f85ad
Thomas Eichinger <thomas.eichinger1@gmail.com>
2014-07-17 15:42:30 +0200

    at86rf231: introduce variable to keep internal driver state
    The at86rf231 radio transceiver provides one IRQ (TRX_END) signaling
    end of transmission or reception but no way to distinguish between
    these.
    
    Conflicts:
    
    	drivers/at86rf231/at86rf231.c

--- drivers/at86rf231/at86rf231.c
+++ drivers/at86rf231/at86rf231.c
@@ -7,6 +7,19 @@
  * details.
  */
 
+/**
+ * @ingroup     drivers_at86rf231
+ * @{
+ *
+ * @file        at86rf231.c
+ * @brief       Driver implementation for at86rf231 chip
+ *
+ * @author      Alaeddine Weslati <alaeddine.weslati@inria.fr>
+ * @author      Thomas Eichinger <thomas.eichinger@fu-berlin.de>
+ *
+ * @}
+ */
+
 #include "at86rf231.h"
 #include "at86rf231_arch.h"
 #include "at86rf231_spi.h"
@@ -18,6 +31,8 @@ static uint16_t radio_pan;
 static uint8_t  radio_channel;
 static uint16_t radio_address;
 static uint64_t radio_address_long;
+
+uint8_t  driver_state;
 
 void at86rf231_init(int tpid)
 {
@@ -88,7 +103,18 @@ void at86rf231_switch_to_rx(void)
 
 void at86rf231_rx_irq(void)
 {
-    at86rf231_rx_handler();
+    /* check if we are in sending state */
+    if (driver_state == AT_DRIVER_STATE_SENDING) {
+        /* Read IRQ to clear it */
+        at86rf231_reg_read(AT86RF231_REG__IRQ_STATUS);
+        /* clear internal state */
+        driver_state = AT_DRIVER_STATE_DEFAULT;
+        return;
+    }
+    else {
+        /* handle receive */
+        at86rf231_rx_handler();
+    }
 }
 
 radio_address_t at86rf231_set_address(radio_address_t address)
