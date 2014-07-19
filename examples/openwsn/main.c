eac32b789e0a90843c9f5002f9cc84aedf27f46d
Thomas Eichinger <thomas.eichinger1@gmail.com>
2014-07-19 17:45:04 +0200

    openwsn: add openwsn example application

---
+++ examples/openwsn/main.c
@@ -0,0 +1,13 @@
+#include "openwsn.h"
+#include "scheduler.h"
+
+int main(void) {
+//   board_init();
+//   scheduler_init();
+    openwsn_start_thread();
+   //  puts("hello?!");
+   // openwsn_init();
+   // puts("and now?!");
+   // scheduler_start();
+   while (1); // this line should never be reached
+}
